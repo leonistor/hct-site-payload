3cfab7bc848cd14660c4580b922e78e7a941dab1
Leo Nistor <leo.nistor@gmail.com>
2025-07-22 22:41:53 +0300

    custom admin component: logo partener

---
+++ src/components/admin/CustomImageCell.tsx
@@ -0,0 +1,34 @@
+// https://dev.to/aaronksaunders/display-images-in-your-payload-cms-admin-collection-list-with-custom-cell-component-8f9
+import React from 'react'
+import Image from 'next/image'
+import { type DefaultServerCellComponentProps } from 'payload'
+
+export const MyComponent = async ({ cellData, payload }: DefaultServerCellComponentProps) => {
+  const media = await payload.findByID({
+    collection: 'media',
+    id: cellData,
+  })
+
+  return (
+    <div
+      style={{
+        position: 'relative',
+        // width: '80px',
+        height: '60px',
+        marginTop: '-5px',
+        marginBottom: '-5px',
+        // transform: 'scale(0.6)',
+        // border: '3px solid',
+      }}
+    >
+      <Image
+        src={media.url!}
+        alt={media.alt}
+        fill
+        style={{
+          objectFit: 'none',
+        }}
+      />
+    </div>
+  )
+}
