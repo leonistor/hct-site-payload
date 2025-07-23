3bd659414581627da14f49ee96e55626981f99d5
Leo Nistor <leo.nistor@gmail.com>
2025-07-23 20:54:12 +0300

    footer stub

--- src/app/(frontend)/sections/Footer.tsx
+++ src/app/(frontend)/sections/Footer.tsx
@@ -1,3 +1,10 @@
+import { Separator } from '@/components/ui/separator'
+import { conf } from '@/config/site'
 export default async function Footer() {
-  return <div>Footer</div>
+  return (
+    <div className="p-2 flex flex-col gap-2 items-center">
+      <Separator className="m-1" />
+      <div className="text-muted-foreground text-sm">&copy; 2025 {conf.title}</div>
+    </div>
+  )
 }
