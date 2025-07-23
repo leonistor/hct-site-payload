7929b4a1113c48303c09f7d61e7d10b16060176e
Leo Nistor <leo.nistor@gmail.com>
2025-07-23 21:38:19 +0300

    demo lista

--- src/app/(frontend)/(pages)/categorii/page.tsx
+++ src/app/(frontend)/(pages)/categorii/page.tsx
@@ -1,3 +1,14 @@
-export default async function Categorii() {
-  return <p>Categorii</p>
+'use client'
+
+import { Separator } from '@/components/ui/separator'
+import DemoLista from '../../components/DemoLista'
+
+export default function Categorii() {
+  return (
+    <div>
+      <p className="text-3xl font-semibold">Categorii</p>
+      <Separator className="m-2" />
+      <DemoLista />
+    </div>
+  )
 }
