9a2ed4cd3d75b15381826d05231a494e2b903397
Leo Nistor <leo.nistor@gmail.com>
2025-07-23 21:29:32 +0300

    page stub clienti

--- src/app/(frontend)/(pages)/clienti/page.tsx
+++ src/app/(frontend)/(pages)/clienti/page.tsx
@@ -1,3 +1,9 @@
+import Testimonial01 from '@/components/testimonial-03/testimonial-03'
+
 export default async function Clienti() {
-  return <p>Clienti</p>
+  return (
+    <>
+      <Testimonial01 />
+    </>
+  )
 }
