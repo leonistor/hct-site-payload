1a2a224912c4a89b37461c2c4c046b0f80b7da16
Leo Nistor <leo.nistor@gmail.com>
2025-07-26 12:13:31 +0300

    fix deploy

--- deploy-02-before-build.sh
+++ deploy-02-before-build.sh
@@ -4,6 +4,7 @@ echo "run before build!"
 
 SECRET=$(openssl rand -base64 24)
 
+mkdir -p .next/standalone
 cp .env.example .next/standalone/.env
 echo "PAYLOAD_SECRET=$SECRET" >>.next/standalone/.env
 
