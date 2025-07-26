f559779f51ec4b36f27dfc3c93346b1ae1c4cb54
Leo Nistor <leo.nistor@gmail.com>
2025-07-26 08:38:31 +0300

    dev: start refactor deploy

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,17 @@
 PROJECT="hct"
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M")
 
-# if [ "$(uname)" != "Darwin" ]; then
-#     echo "Does not work on Linux! Must change 'cp -r' to 'cp -R'! Aborting"
-#     exit 1
-# fi
-
 # copy files and sqlite db
-cp -r public .next/standalone/
-cp -r .next/static .next/standalone/.next/
-cp -r db .next/standalone/
-echo "Copied files and sqlite db to .next/standalone"
+# cp -r public .next/standalone/
+# #!!! ???
+# cp -r .next/static .next/standalone/.next/
+# cp -r db .next/standalone/
+# echo "Copied files and sqlite db to .next/standalone"
 
-NAME="$PROJECT-$TIMESTAMP"
+NAME="$PROJECT-data-$TIMESTAMP"
 
 mkdir -p build
-tar -C .next -czf "build/$NAME.tar.gz" standalone/
+# tar -C .next -czf "build/$NAME.tar.gz" standalone/
+tar -czf "build/$NAME.tar.gz" public db
 
 echo "Done archive at: build/$NAME.tar.gz"
