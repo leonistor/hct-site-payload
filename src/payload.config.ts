ebe1368cda0e3c8874d2e058739ea9c7a06d261f
Leo Nistor <leo.nistor@gmail.com>
2025-07-26 17:44:40 +0300

    hack cors, csrf in payload config

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -37,8 +37,8 @@ export default buildConfig({
   collections: [Users, Media, Parteneri, Produse, Categorii, ImaginiProduse, Materiale],
   editor: lexicalEditor(),
   // cors: '*',
-  csrf: ['http://localhost:3000'],
-  cors: ['http://localhost:3000'],
+  csrf: ['http://localhost:3000', 'http://pop-os.lan:3000', 'http://coca.lan:3000'],
+  cors: ['http://localhost:3000', 'http://pop-os.lan:3000', 'http://coca.lan:3000'],
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
