82d1124ee8b83e55700209f09f0c847ed23d69af
Leo Nistor <leo.nistor@gmail.com>
2025-07-31 21:22:48 +0300

    pagina produs start

--- src/app/(frontend)/(pages)/produs/[id]/page.tsx
+++ src/app/(frontend)/(pages)/produs/[id]/page.tsx
@@ -1,5 +1,8 @@
 import { getPayload } from 'payload'
 import config from '@payload-config'
+import { Categorii, Imgprod, Parteneri } from '@/payload-types'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default async function Produs({ params }: { params: Promise<{ id: string }> }) {
   const payload = await getPayload({ config })
@@ -10,9 +13,43 @@ export default async function Produs({ params }: { params: Promise<{ id: string 
     id: id,
   })
 
+  if (!prod) {
+    return <div>product id {id} not found!</div>
+  }
+
+  const { nume, categorie, materiale, url_producator, descriere, descriere_md, imagini } = prod
+
+  const partener = prod.partener as Parteneri
+  const default_img = prod.default_img as Imgprod
+
+  let categorie_denumire, categorie_id
+  if (categorie) {
+    categorie_denumire = (categorie as Categorii).denumire
+    categorie_id = (categorie as Categorii).id
+  }
+  const poza_src = default_img.sizes!.thumbnail!.url!
+  const poza_width = default_img.sizes!.thumbnail!.width!
+  const poza_height = default_img.sizes!.thumbnail!.height!
+
   return (
-    <div>
-      <code>{JSON.stringify(prod)}</code>
+    <div className="flex flex-row pt-4 gap-8">
+      <div className="flex-2/3 flex flex-col gap-4">
+        <h2 className="text-3xl leading-10 sm:text-4xl md:text-[40px] md:leading-[3.25rem] font-bold tracking-tight">
+          {nume}
+        </h2>
+        <p>{descriere}</p>
+        <p>Partener: {partener.nume}</p>
+        {/* <p>{categorie as string}</p> */}
+        <code>{url_producator}</code>
+        <div>
+          Categorie: <Link href={`/categorii/${categorie_id}`}>{categorie_denumire}</Link>
+        </div>
+        <div>Materiale:</div>
+      </div>
+      {/*  */}
+      <div className="flex-1/3">
+        <Image src={poza_src} alt="" width={poza_width} height={poza_height} />
+      </div>
     </div>
   )
 }
